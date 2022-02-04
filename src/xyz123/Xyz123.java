01b8aeb642a525135a34c73a267d407e5e870a98
XinwenWatson <lixinwe@sheridancollege.ca>
2022-02-04 08:24:09 -0500

    Fifth commit: add PartTimeStudent Class

---
+++ src/xyz123/Xyz123.java
@@ -0,0 +1,17 @@
+package xyz123;
+
+/**
+ * This class +++Insert Description Here+++
+ * 
+ * @author Xinwen Li
+ */
+public class Xyz123 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
